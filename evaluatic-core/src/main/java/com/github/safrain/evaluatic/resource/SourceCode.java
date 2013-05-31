db2c83bb12e9f14b96e918704b003a360a6e5f24
safrain <z.safrain@gmail.com>
2013-05-31 18:26:32 +0800

    Initial import

---
+++ evaluatic-core/src/main/java/com/github/safrain/evaluatic/resource/SourceCode.java
@@ -0,0 +1,19 @@
+package com.github.safrain.evaluatic.resource;
+
+/**
+ * Script source code used by framework and user applications
+ *
+ * @author Safrain
+ */
+public class SourceCode {
+    private String name;
+    private String source;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ evaluatic-core/src/main/java/com/github/safrain/evaluatic/resource/SourceCodeRepository.java
@@ -0,0 +1,107 @@
+package com.github.safrain.evaluatic.resource;
+
+import java.util.List;
+
+/**
+ * SourceCode container
+ *
+ * @author safrain
+ */
+public interface SourceCodeRepository {
+
+    /**
+     * Indicate if this repository contains source code with given name
+     *
+     * @param name Name of the source code
+     * @return
+     */
+    boolean hasSourceCode(String name);
+
+    /**
+     * Return source code with given name
+     *
+     * @param name Name of the source code
+     * @return Resource with given name
+     * @throws SourceCodeNotExistException If requested source code does not exist
+     */
+    SourceCode get(String name) throws SourceCodeNotExistException;
+
+    /**
+     * Save given source code in this respository
+     *
+     * @param sourceCode SourceCode to save
+     */
+    void create(SourceCode sourceCode) throws SourceCodeModificationFailedException;
+
+    /**
+     * Update given source code
+     *
+     * @param sourceCode SourceCode to update
+     * @throws SourceCodeModificationFailedException
+     *          If update failed
+     */
+    void update(SourceCode sourceCode) throws SourceCodeModificationFailedException;
+
+    /**
+     * Delete source code by name
+     *
+     * @param name SourceCode to delete
+     * @throws SourceCodeModificationFailedException
+     *          If delete failed
+     */
+    void delete(String name) throws SourceCodeModificationFailedException;
+
+    /**
+     * Return all source code in this Repository
+     *
+     * @return All source code list
+     */
+    List<SourceCode> list();
+
+
+    /**
+     * Thrown if requested source code does not exists in SourceCodeRepository
+     *
+     * @author safrain
+     */
+    public class SourceCodeNotExistException extends RuntimeException {
+        public SourceCodeNotExistException() {
+        }
+
+        public SourceCodeNotExistException(String message) {
+            super(message);
+        }
+
+        public SourceCodeNotExistException(String message, Throwable cause) {
+            super(message, cause);
+        }
+
+        public SourceCodeNotExistException(Throwable cause) {
+            super(cause);
+        }
+    }
+
+
+    /**
+     * Thrown if create/update/delete is failed
+     *
+     * @author safrain
+     */
+    public class SourceCodeModificationFailedException extends RuntimeException {
+        public SourceCodeModificationFailedException() {
+        }
+
+        public SourceCodeModificationFailedException(String message) {
+            super(message);
+        }
+
+        public SourceCodeModificationFailedException(String message, Throwable cause) {
+            super(message, cause);
+        }
+
+        public SourceCodeModificationFailedException(Throwable cause) {
+            super(cause);
+        }
+    }
+
+}
